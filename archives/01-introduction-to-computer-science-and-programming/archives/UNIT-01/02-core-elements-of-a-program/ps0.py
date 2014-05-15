a38165d77895a8f8853cc1b069a7f9a449a63879
Eric Douglas <eric.eoop@gmail.com>
2014-05-14 21:25:01 -0300

    fix typo ps0.py

--- archives/01-introduction-to-computer-science-and-programming/archives/UNIT-01/02-core-elements-of-a-program/ps0.py
+++ archives/01-introduction-to-computer-science-and-programming/archives/UNIT-01/02-core-elements-of-a-program/ps0.py
@@ -3,6 +3,6 @@
 # Time Spent: 3:10
 
 birthday = raw_input('When is your birthday?\n')
-lastName = raw_input('What is yout last name?\n')
+lastName = raw_input('What is your last name?\n')
 
 print lastName + ' ' + birthday
