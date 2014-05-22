62b7aa99677ccadca1f30a9004f7f02509ab953e
Eric Douglas <eric.eoop@gmail.com>
2014-05-22 18:38:48 -0300

    add counting and filtered-count patterns in 1.1.3

--- archives/01-introduction-to-computer-science-and-programming/archives/UNIT-01/03-problem-solving/loops.py
+++ archives/01-introduction-to-computer-science-and-programming/archives/UNIT-01/03-problem-solving/loops.py
@@ -1,1 +1,23 @@
 # Loops - http://beastie.cs.ua.edu/cs150/book/index_14.html
+
+i = 0
+while i < 5:
+  print i
+  i += 1
+
+# Other loops
+for i in range(0, 5, 1): # 5 exclusive
+  print i
+
+# The counting pattern
+count = 0
+for i in range(0, len(items), 1):
+  count += 1
+
+# The filtered-count pattern
+count = 0
+for i in range(0, len(items), 1):
+  if items[i] % 2 == 0:
+    count += 1
+
+
