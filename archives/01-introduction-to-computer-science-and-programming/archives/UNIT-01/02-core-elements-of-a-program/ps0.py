b49c27c6a6a89a6ab6be10ed39874e97aa95048e
ericdouglas <eric.douglas.mail@gmail.com>
2015-05-13 15:16:49 -0300

    Remove old files

--- archives/01-introduction-to-computer-science-and-programming/archives/UNIT-01/02-core-elements-of-a-program/ps0.py
+++
@@ -1,8 +0,0 @@
-# Problem Set 0
-# Name: Eric Douglas
-# Time Spent: 3:10
-
-birthday = raw_input('When is your birthday?\n')
-lastName = raw_input('What is your last name?\n')
-
-print lastName + ' ' + birthday
